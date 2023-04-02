e0e046ec12460b5d4ccb1920ba5f158a2b38c24a
Aliaksei Rudak <lexarudak@gmail.com>
2023-04-02 18:00:57 +0600

    feat: change summury text

--- src/ts/htmlSections/summary.ts
+++ src/ts/htmlSections/summary.ts
@@ -1,17 +1,18 @@
 const summary = `<div class="cv-card">
-                    <p>32 year old aspiring JavaScript Frontend developer with extensive sales experience. My work experience is not related to the IT field, but helped me acquire such important skills as:</p>
+                    <p>Hello, I am a beginner JavaScript front-end developer. I recently completed a JavaScript/Front-end course from RS School and am currently taking a React course. My work experience in other areas helped me finish the course with a fairly high place. I was a team leader in two team projects and both works were included in the top list.</p>
+                    <p>Here are what I now consider my most useful skills:</p>
                     <ul class="list">
                       <li class="list__item">
-                        Ability to focus on completing a task
+                        I start the task immediately after it is issued
                       </li>
                       <li class="list__item">
-                        Work under stress without loss of quality
+                        Ability to read and understand technical specifications
                       </li>
                       <li class="list__item">
-                        Break down a big task into separate steps and do it gradually, without panic and fuss
+                        Ability to organize team work
                       </li>
                        <li class="list__item">
-                        Capture the essence of the customer's wishes
+                        Able to work hard and compete with more experienced guys
                       </li>
                        <li class="list__item">
                         Communicate in an understandable language with people of different status and qualifications
