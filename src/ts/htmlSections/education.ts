ebf2b66904d705af56a0f6bab2c3fc8a9a97977f
Aliaksei Rudak <lexarudak@gmail.com>
2023-04-02 15:49:41 +0600

    feat: add RS School react info

--- src/ts/htmlSections/education.ts
+++ src/ts/htmlSections/education.ts
@@ -1,13 +1,21 @@
 const education = `<div class="cv-card">
+                    <p class="cv-card__title">RS School | React</p>
+                    <p class="cv-card__date">
+                      March 2023 - Present <span class="cv-card__location">| Almaty, Kazakhstan</span>
+                    </p>
+                    <p class="cv-card__post">Position: In progress</p>
+                  </div>
+                  <div class="cv-card">
                     <p class="cv-card__title">RS School | JavaScript/Front-end</p>
                     <p class="cv-card__date">
                       September 2022 - March 2023 <span class="cv-card__location">| Almaty, Kazakhstan</span>
                     </p>
-                    <a class="button" href="https://app.rs.school/certificate/6yaeqs17" target="_blank">
+                    <a class="button" href="https://app.rs.school/certificate/aubp6e79" target="_blank">
                     Certificate
                     </a>
                     <p class="cv-card__post">Position: 35</p>
-                  </div><div class="cv-card">
+                  </div>
+                  <div class="cv-card">
                     <p class="cv-card__title">Belarusian State Academy of Communications</p>
                     <p class="cv-card__date">
                       September 2007 - June 2014 <span class="cv-card__location">| Minsk, Belarus</span>
