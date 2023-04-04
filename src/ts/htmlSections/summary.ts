fa59dd873eedc46861afe0d26e0c3e547b40772d
Aliaksei Rudak <lexarudak@gmail.com>
2023-04-04 08:24:13 +0600

    feat: change summary section

--- src/ts/htmlSections/summary.ts
+++ src/ts/htmlSections/summary.ts
@@ -1,5 +1,5 @@
 const summary = `<div class="cv-card">
-                    <p>Hello, I am a beginner JavaScript front-end developer. I recently completed a JavaScript/Front-end course from RS School and am currently taking a React course. My work experience in other areas helped me finish the course with a fairly high place. I was a team leader in two team projects and both works were included in the top list.</p>
+                    <p>I am a beginner JavaScript front-end developer. I recently completed a JavaScript/Front-end course from RS School and am currently taking a React course. My work experience in other areas helped me finish the course with a fairly high place. I was a team leader in two team projects and both works were included in the top list.</p>
                     <p>Here are what I now consider my most useful skills:</p>
                     <ul class="list">
                       <li class="list__item">
